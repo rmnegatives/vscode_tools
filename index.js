995fad1a2f994e5c79e2a7d99ec6f4ee5954036d
Imran Brown <imraa2@yahoo.com>
2017-12-17 18:29:05 -0500

    feature: add prettier,precommit, and husky

--- index.js
+++ index.js
@@ -1,81 +1,78 @@
 #!/usr/bin/env node
 
-const chalk = require( 'chalk' );
+const chalk = require('chalk');
 
 const info = chalk.blue;
 const printError = chalk.bold.red;
 const printSuccess = chalk.green;
 
-const fs = require( 'fs' );
+const fs = require('fs');
 
-const exec = require( 'child_process' ).exec;
-const argv = require( 'yargs' )
-  .usage( 'Usage: $0 <command> [options]' )
-  .example( '$0 -i -f vscode_extensions.txt', 'Install all vscode extensions' )
-  .example( '$0 -i -dev -f vscode_extensions.txt', 'Install all vscode extensions with insiders' )
-  .example( '$0 -e', 'Export all vscode extensions' )
-  .example( '$0 -d', 'Disable all vscode extensions' )
-  .alias( 'dev', 'vscodeDev' )
-  .alias( 'f', 'file' )
-  .nargs( 'f', 1 )
-  .describe( 'd', 'Disable all vscode extensions' )
-  .describe( 'dev', 'Use vscode insiders' )
-  .describe( 'e', 'Export all vscode extensions to vscode_extensions.txt' )
-  .describe( 'f', 'Load a file' )
-  .describe( 'i', 'Install all vscode extensions' )
-  .help( 'h' )
-  .epilog( 'copyright 2017' )
-  .argv;
+const exec = require('child_process').exec;
+const argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .example('$0 -i -f vscode_extensions.txt', 'Install all vscode extensions')
+    .example('$0 -i -dev -f vscode_extensions.txt', 'Install all vscode extensions with insiders')
+    .example('$0 -e', 'Export all vscode extensions')
+    .example('$0 -d', 'Disable all vscode extensions')
+    .alias('dev', 'vscodeDev')
+    .alias('f', 'file')
+    .nargs('f', 1)
+    .describe('d', 'Disable all vscode extensions')
+    .describe('dev', 'Use vscode insiders')
+    .describe('e', 'Export all vscode extensions to vscode_extensions.txt')
+    .describe('f', 'Load a file')
+    .describe('i', 'Install all vscode extensions')
+    .help('h')
+    .epilog('copyright 2017').argv;
 
 const vsCodeCommandLine = {
-  install: '--install-extension',
-  disable: '--disable-extensions',
-  list: '--list-extensions',
+    install: '--install-extension',
+    disable: '--disable-extensions',
+    list: '--list-extensions',
 };
 
 let vscodeVersion = 'code';
 let vsCodeExecute = '';
 
-if ( argv.vscodeDev ) {
-  vscodeVersion = 'code-insiders';
+if (argv.vscodeDev) {
+    vscodeVersion = 'code-insiders';
 }
 
-if ( argv.d ) {
-  const disableCommand = `${vscodeVersion} ${vsCodeCommandLine.disable}`;
-  exec( disableCommand, execResult );
-  return;
+if (argv.d) {
+    const disableCommand = `${vscodeVersion} ${vsCodeCommandLine.disable}`;
+    exec(disableCommand, execResult);
+    return;
 }
 
-if ( argv.e ) {
-  const exportCommand = `${vscodeVersion} ${vsCodeCommandLine.list} >> vscode_extensions.txt`;
-  exec( exportCommand, execResult );
-  return;
+if (argv.e) {
+    const exportCommand = `${vscodeVersion} ${vsCodeCommandLine.list} >> vscode_extensions.txt`;
+    exec(exportCommand, execResult);
+    return;
 }
 
-if ( argv.f && argv.i ) {
-  let extensions = [];
-  const vsCodeExecute = `${vscodeVersion} ${vsCodeCommandLine.install}`;
-  const fileStream = fs.createReadStream( argv.file );
-  fileStream.on( 'data', function ( buf ) {
-    const extension = buf.toString();
-    extensions = extension.toString().split( "\n" );
-  } );
+if (argv.f && argv.i) {
+    let extensions = [];
+    const vsCodeExecute = `${vscodeVersion} ${vsCodeCommandLine.install}`;
+    const fileStream = fs.createReadStream(argv.file);
+    fileStream.on('data', function(buf) {
+        const extension = buf.toString();
+        extensions = extension.toString().split('\n');
+    });
 
-  fileStream.on( 'end', () => {
-    extensions.forEach( ( val, i ) => {
-      const command = `${vsCodeExecute} ${val}`;
-      console.log( info( `Installing extension: ${val}` ) );
+    fileStream.on('end', () => {
+        extensions.forEach((val, i) => {
+            const command = `${vsCodeExecute} ${val}`;
+            console.log(info(`Installing extension: ${val}`));
 
-      exec( command, execResult );
-    } );
-    console.log( printSuccess( 'All Extensions Installed' ) );
-  } );
+            exec(command, execResult);
+        });
+        console.log(printSuccess('All Extensions Installed'));
+    });
 
-  function execResult( error, stdout, stderr ) {
-    if ( error ) {
-      console.log( printError( `exec error: ${error}` ) );
+    function execResult(error, stdout, stderr) {
+        if (error) {
+            console.log(printError(`exec error: ${error}`));
+        }
     }
-  }
-
-
 }
